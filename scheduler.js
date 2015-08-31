e182cd1d112e0ec19c2f6e067ab5dbee0cb5b4e0
Jonathan Chang <jochang@cs.hmc.edu>
2015-08-31 10:16:22 -0700

    Implemented tostring

--- scheduler.js
+++ scheduler.js
@@ -228,12 +228,36 @@ function formatDate(date) {
 }
 
 function VEventObject(args) {
-    this.weekdays = [];
-    this.start = 0.0;
-    this.end = 0.0;
+    this.weekday = 0;
+    this.startTime = 0.0;
+    this.endTime = 0.0;
+	this.startDate = null;
+	this.endDate = null;
     this.name = '';
     this.loc = '';
-    this.toString = function () { return '' }
+    this.toString = function () {
+		var days = ['MO', 'TU', 'WE', 'TH', 'FR'];
+		var startDateFull = dateAddHoursAndMinutes(startDate, startTime);
+		var endDateFull = dateAddHoursAndMinutes(endDate, endTime);
+		var header = 'BEGIN:VEVENT\n';
+		var footer = 'END:VEVENT\n';
+		var uid = 'UID:' + startDate + startTime + '-' + (new Date()).getTime() + '\n';
+		var dtstart = 'DTSTART:' + formatDate(startDateFull) + '\n';
+		var dtend = 'DTEND:' + formatDate(endDateFull) + '\n';
+  		var dtstamp = 'DTSTAMP:' + formatDate(new Date()) + '\n';
+		var rrule = 'RRULE:FREQ=WEEKLY;BYDAY=' + days[weekday] + ';UNTIL=' + formatDate(endDate) + '\n';
+		return header + uid + dtstart + dtend + dtend + dtstamp + loc + rrule + name + footer;
+	}
+}
+
+function dateAddHoursAndMinutes(date, fracHours)
+{
+	var hours = Math.floor(fracHours);
+	var minutes = (fracHours - hours) * 60;
+	var newDate = new Date(date);
+	newDate.setHours(hours);
+	newDate.setMinutes(minutes);
+	return newDate;
 }
 
 function buildVEvent(timeBlock, i) {
